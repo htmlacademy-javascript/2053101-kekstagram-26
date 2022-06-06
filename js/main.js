584971a854f7ec224deb55c27b27cc3692e2bf2c
ViktorLukashevich <viktor.lukashevich@gmail.com>
2022-06-06 23:42:02 +0300

    Написал две функции (случайное число из диапазона, проверка длины строки) в main.js

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Источник функции https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const result = (min >= max) ? 'Ошибка! min больше или равно max' :
+    (min < 0) ? 'Ошибка! Числа должны быть больше 0' :
+      Math.floor(Math.random() * (max - min + 1) + min);
+  return result;
+}
+
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength ? true : false;
+}
