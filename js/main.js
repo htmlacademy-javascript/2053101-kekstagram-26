df828f37989e54b49877a43457df853244ebf88c
ViktorLukashevich <viktor.lukashevich@gmail.com>
2022-06-07 13:55:57 +0300

    Сделал все функции стрелочными

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-nested-ternary */
 // Источник функции https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   const result = (min >= max) ? 'Ошибка! min больше или равно max' :
@@ -10,7 +10,5 @@ function getRandomIntInclusive(min, max) {
   return result;
 }
 getRandomIntInclusive(1,5);
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
-checkStringLength('hello', 2);
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+console.log(checkStringLength('hello', 14));
