4377e80b8c3ba12f8dd3ca1e27432f76ace0ce01
Keks <keks@htmlacademy.ru>
2022-06-26 20:20:29 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/create-photo.js
@@ -0,0 +1,20 @@
+import { createPhotoAlbum } from './create-photo-album.js';
+
+const pictureTemplate = document.querySelector('#picture').content.querySelector('a');
+const photoAlbum = createPhotoAlbum();
+const photoAlbumFragment = document.createDocumentFragment();
+const picturesContainer = document.querySelector('.pictures');
+
+const createPhoto = () => {photoAlbum.forEach((element, index) => {
+  const newPicture = pictureTemplate.cloneNode(true);
+  newPicture.querySelector('.picture__img').src = element.url;
+  newPicture.querySelector('.picture__likes').textContent = element.likes;
+  newPicture.querySelector('.picture__comments').textContent = element.comments.length;
+  // Добавляем data-атрибут картинке
+  newPicture.dataset.index = index;
+  photoAlbumFragment.append(newPicture);
+});
+picturesContainer.append(photoAlbumFragment);
+};
+
+export {createPhoto, photoAlbum};
---
+++ 6/js/main.js
@@ -0,0 +1,3 @@
+import { createBigPhoto } from './create-big-photo.js';
+
+createBigPhoto();
