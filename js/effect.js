a78ba4037666879e235a1609be7507d12219f35c
ViktorLukashevich <viktor.lukashevich@gmail.com>
2022-07-06 15:04:41 +0300

    Добавил обработчик на слайдер

--- js/effect.js
+++ js/effect.js
@@ -1,4 +1,23 @@
+
+const effectsList = document.querySelector('.effects__list');
+const imgUploadPreview = document.querySelector('.img-upload__preview');
+
+// Обработчик добавляет модификатор к картинке
+const onEffectListClick = (evt) => {
+  const currentElement = evt.target;
+  if (currentElement.tagName === 'INPUT') {
+    const effectPreviewModificator = currentElement.closest('li').querySelector('span').classList[1]; // определяем какой модификатор нужно добавить к картинке
+    if (imgUploadPreview.classList.length > 1) { // если модификаторов у картинки больше чем 1
+      imgUploadPreview.classList.remove(imgUploadPreview.classList[1]); // то удаляем 2-й
+    }
+    imgUploadPreview.classList.add(effectPreviewModificator);
+  }
+};
+// Добавляем обработчик на click по картинке
+effectsList.addEventListener('click', (evt) => onEffectListClick(evt));
+
 const sliderElement = document.querySelector('.effect-level__slider');
+const valueElement = document.querySelector('.effect-level__value');
 
 noUiSlider.create(sliderElement, {
   range: {
@@ -10,23 +29,8 @@ noUiSlider.create(sliderElement, {
   connect: 'lower',
 });
 
-sliderElement.noUiSlider.on('update', (...rest) => {
-    // console.log(rest);
+sliderElement.noUiSlider.on('update', () => {
+  valueElement.value = sliderElement.noUiSlider.get();
+  console.log(valueElement.value)
 });
 
-
-const effectsList = document.querySelector('.effects__list');
-const imgUploadPreview = document.querySelector('.img-upload__preview');
-
-const onEffectListClick = (evt) => {
-  const currentElement = evt.target;
-  if (currentElement.tagName === 'INPUT') {
-    const effectPreviewModificator = currentElement.closest('li').querySelector('span').classList[1]; // определяем какой модификатор нужно добавить к картинке
-    if (imgUploadPreview.classList.length > 1) { // если модификаторов у картинки больше чем 1
-      imgUploadPreview.classList.remove(imgUploadPreview.classList[1]); // то удаляем 2-й
-    }
-    imgUploadPreview.classList.add(effectPreviewModificator);
-  }
-};
-// Добавляет модификатор к картинке
-effectsList.addEventListener('click', (evt) => onEffectListClick(evt));
