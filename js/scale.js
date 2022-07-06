d4b4488be5dfa917f5ec2bd0d87e282b7cea67a8
ViktorLukashevich <viktor.lukashevich@gmail.com>
2022-07-06 21:39:42 +0300

    Добавил модуль scale, все работает

---
+++ js/scale.js
@@ -0,0 +1,21 @@
+const scaleControlSmaller = document.querySelector('.scale__control--smaller');
+const scaleControlBigger = document.querySelector('.scale__control--bigger');
+const scaleControlValue = document.querySelector('.scale__control--value');
+let scaleControlNumber = Number(scaleControlValue.value.replace(/%/g,''));
+const imgUploadPreview = document.querySelector('.img-upload__preview');
+
+scaleControlBigger.addEventListener('click', () => {
+  if (scaleControlNumber < 100) {
+    scaleControlNumber += 25;
+    scaleControlValue.value = `${ scaleControlNumber }%`;
+    imgUploadPreview.style.transform = 'scale(' + scaleControlNumber / 100 + ')';
+  }
+});
+
+scaleControlSmaller.addEventListener('click', () => {
+  if (scaleControlNumber > 25) {
+    scaleControlNumber -= 25;
+    scaleControlValue.value = `${ scaleControlNumber }%`;
+    imgUploadPreview.style.transform = 'scale(' + scaleControlNumber / 100 + ')';
+  }
+});
