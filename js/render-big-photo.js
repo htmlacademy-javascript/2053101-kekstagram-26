0a2ecbc9c86983dd862a8ba830dd5dfa88159192
ViktorLukashevich <viktor.lukashevich@gmail.com>
2022-07-11 21:00:41 +0300

    Рефакторинг render-big-photo

--- js/render-big-photo.js
+++ js/render-big-photo.js
@@ -3,16 +3,19 @@ import {isEsc, createNewElement} from './util.js';
 import { SOCIAL_COMMENT_COUNT } from './data.js';
 
 const picturesSection = document.querySelector('.pictures');
+const bigPicture = document.querySelector('.big-picture');
+const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
+const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
+const likesCount = document.querySelector('.likes-count');
+const socialComments = document.querySelector('.social__comments');
+const socialCaptions = document.querySelector('.social__caption');
+const socialCommentCount = document.querySelector('.social__comment-count'); // Контейнер для кол-ва комментариев
+const commentsCount = document.querySelector('.comments-count');
+const commentsLoader = document.querySelector('.comments-loader');
 
 const renderBigPhoto = (photos) => {
-  renderPhotos(photos);
 
-  const bigPicture = document.querySelector('.big-picture');
-  const bigPictureImg = bigPicture.querySelector('.big-picture__img').querySelector('img');
-  const bigPictureCloseButton = bigPicture.querySelector('.big-picture__cancel');
-  const likesCount = document.querySelector('.likes-count');
-  const socialComments = document.querySelector('.social__comments');
-  const socialCaptions = document.querySelector('.social__caption');
+  renderPhotos(photos);
 
   // Обработчик esc
   const onBigPictureEscKeydown = (evt) => {
@@ -25,12 +28,7 @@ const renderBigPhoto = (photos) => {
   // Функция открытия большой картинки
   function openBigPicture () {
     bigPicture.classList.remove('hidden');
-
-    const commentsLoader = document.querySelector('.comments-loader');
-
-    commentsLoader.classList.add('hidden');
     document.body.classList.add('modal-open');
-    // Добавляем обработчик esc
     document.addEventListener('keydown', onBigPictureEscKeydown);
   }
 
@@ -38,8 +36,30 @@ const renderBigPhoto = (photos) => {
   function closeBigPicture ()  {
     bigPicture.classList.add('hidden');
     document.body.classList.remove('modal-open');
-    // Удаляем обработчик esc
     document.removeEventListener('keydown', onBigPictureEscKeydown);
+  }
+
+  // Кол-во комментариев в отображаемом фото
+  const socialCommentCountValue = (comments) => {
+    if(comments > SOCIAL_COMMENT_COUNT) {
+      return SOCIAL_COMMENT_COUNT;
+    }
+    return comments;
+  };
+
+  // Функция отрисовывает комментарии к большой фотографии
+  const renderComments = (commentsAmount) => {
+    for(let i = 0; i < socialCommentCountValue(commentsAmount); i++) { // В условии кол-во отображаемых комментариев (до 5-ти вкл.)
+      const socialComment = createNewElement('li','social__comment');
+      const socialPicture = createNewElement('img','social__picture');
+      socialPicture.src = item.comments[i].avatar;
+      socialPicture.alt = item.comments[i].name;
+      socialPicture.width = '35';
+      socialPicture.height = '35';
+      const socialText = createNewElement('p', 'social__text', item.comments[i].message);
+      socialComment.append(socialPicture);
+      socialComment.append(socialText);
+      socialComments.append(socialComment);
   }
 
   bigPictureCloseButton.addEventListener('click', () => closeBigPicture());
@@ -49,34 +69,25 @@ const renderBigPhoto = (photos) => {
     const currentElement = evt.target; // Объект, на который кликнули
 
     if(currentElement.classList.contains('picture__img')) {
-
-      const socialCommentCount = document.querySelector('.social__comment-count'); // Контейнер для кол-ва комментариев
+      // Считываем данные из картинки, по которой кликнули
       const currentIndex = Number(currentElement.closest('a').dataset.index); // Текущий индекс элемента из data-атрибута
       const likesCountNumber = currentElement.closest('a').querySelector('.picture__likes').textContent; // Кол-во лайков
       const commentsCountAmount = currentElement.closest('a').querySelector('.picture__comments').textContent; // Кол-во комментариев всего
-      const commentsCount = document.querySelector('.comments-count');
 
-      // Кол-во комментариев в preview
-      const socialCommentCountValue = (comments) => {
-        if(comments > SOCIAL_COMMENT_COUNT) {
-          return SOCIAL_COMMENT_COUNT;
-        }
-        return comments;
-      };
-
+      // Записываем данные в большую картинку и комментарии к ней
       bigPictureImg.src = currentElement.src;
       likesCount.textContent = likesCountNumber;
       commentsCount.textContent = commentsCountAmount;
-
       socialCommentCount.textContent = `${ socialCommentCountValue(commentsCountAmount) } из `;
       socialCommentCount.append(commentsCount);
 
-      socialComments.textContent = ''; // Очистили шаблонные комментарии
+      // Очищаем шаблонные комментарии
+      socialComments.textContent = '';
 
-      // Находим объект по index, чтобы создать комментарии, описание и т.д. к картинке
+      // Находим объект по index, чтобы создать комментарии, описание и т.д. к большой картинке
       photos.forEach((item, index) => {
         if(index === currentIndex) {
-          for(let i = 0; i < socialCommentCountValue(commentsCountAmount); i++) {
+          for(let i = 0; i < socialCommentCountValue(commentsCountAmount); i++) { // В условии кол-во отображаемых комментариев (до 5-ти вкл.)
             const socialComment = createNewElement('li','social__comment');
             const socialPicture = createNewElement('img','social__picture');
             socialPicture.src = item.comments[i].avatar;
