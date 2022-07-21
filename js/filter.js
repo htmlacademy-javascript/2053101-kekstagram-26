185479755877090069ef93b9104545564a0a2cf5
ViktorLukashevich <viktor.lukashevich@gmail.com>
2022-07-21 14:30:49 +0300

    Корректировка filter.js согласно критерию Д23

--- js/filter.js
+++ js/filter.js
@@ -8,41 +8,47 @@ const imgFiltersElement = document.querySelector('.img-filters');
 const filterDefaultElement = document.querySelector('#filter-default');
 const filterRandomElement = document.querySelector('#filter-random');
 const filterDiscussedElement = document.querySelector('#filter-discussed');
+const filters = document.querySelectorAll('.img-filters__button');
+const activeButton = 'img-filters__button--active';
 
 // Функция для сортировки фото по убыванию комментариев
 function compareCommentNumbers(a, b) {
   return b.comments.length - a.comments.length;
 }
 
+// Функция удаляет класс активного фильтра с кнопки
+const removeCurrentFilter = () => {
+  filters.forEach((element) => {
+    if(element.classList.contains(activeButton)) {
+      element.classList.remove(activeButton);
+    }
+  });
+};
+
 // Функция возвращает массив фото исходя из нажатой кнопки
-function getFilteredPhotos (photos, choosenFilter) {
+const getFilteredPhotos = (photos, choosenFilter) => {
   let filteredPhotos = [];
   return function () {
     switch(choosenFilter) {
       case filterDefaultElement:
-        filterDefaultElement.classList.add('img-filters__button--active');
-        filterRandomElement.classList.remove('img-filters__button--active');
-        filterDiscussedElement.classList.remove('img-filters__button--active');
+        filterDefaultElement.classList.add(activeButton);
         filteredPhotos = photos.slice();
         break;
       case filterRandomElement:
-        filterRandomElement.classList.add('img-filters__button--active');
-        filterDefaultElement.classList.remove('img-filters__button--active');
-        filterDiscussedElement.classList.remove('img-filters__button--active');
+        filterRandomElement.classList.add(activeButton);
         for(let i = 0; i <= RANDOM_PHOTOS; i++) {
           filteredPhotos.push(getRandomArrayElement(photos));
         }
         break;
       case filterDiscussedElement:
-        filterDiscussedElement.classList.add('img-filters__button--active');
-        filterDefaultElement.classList.remove('img-filters__button--active');
-        filterRandomElement.classList.remove('img-filters__button--active');
+        filterDiscussedElement.classList.add(activeButton);
+
         filteredPhotos = photos.slice().sort(compareCommentNumbers);
         break;
     }
     return filteredPhotos;
   }();
-}
+};
 
 const setImgFilters = (photos) => {
 
@@ -55,8 +61,8 @@ const setImgFilters = (photos) => {
       !choosenFilter.classList.contains('img-filters__button')) {
       return;
     }
+    removeCurrentFilter();
     const filteredPhotos = getFilteredPhotos(photos, choosenFilter);
-
     // Отрисовываем отфильтрованные фото
     debounce(() => renderPhotos(filteredPhotos), RERENDER_DELAY)();
 
